15f772c6dc1ac0c82f0faeb95640225dc4b03447
shareChong <815898148@qq.com>
2019-09-26 09:03:48 +0800

    电影服务

--- huangniuniu-movie/huangniuniu-movie-interface/src/main/java/com/huangniuniu/movie/pojo/Movie.java
+++ huangniuniu-movie/huangniuniu-movie-interface/src/main/java/com/huangniuniu/movie/pojo/Movie.java
@@ -1,21 +1,23 @@
 package com.huangniuniu.movie.pojo;
 
-import tk.mybatis.mapper.annotation.NameStyle;
-import tk.mybatis.mapper.code.Style;
+
+import lombok.Data;
 
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
 import java.util.Date;
 
-@Table(name = "tb_movie")
-@NameStyle(Style.normal)
+@Data
+@Table(name = "movie")
 public class Movie {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer movieid;
+    private Long id;
+    @NotBlank(message = "电影名不能为空")
     private String movieName;
     private String movieType;
     private String location;
@@ -29,99 +31,5 @@ public class Movie {
     private Date soldOutTime;
 
 
-    public Integer getMovieid() {
-        return movieid;
-    }
 
-    public void setMovieid(Integer movieid) {
-        this.movieid = movieid;
-    }
-
-    public String getMovieName() {
-        return movieName;
-    }
-
-    public void setMovieName(String movieName) {
-        this.movieName = movieName;
-    }
-
-    public String getMovieType() {
-        return movieType;
-    }
-
-    public void setMovieType(String movieType) {
-        this.movieType = movieType;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
-    }
-
-    public String getDiscription() {
-        return discription;
-    }
-
-    public void setDiscription(String discription) {
-        this.discription = discription;
-    }
-
-    public String getMoviePicture() {
-        return moviePicture;
-    }
-
-    public void setMoviePicture(String moviePicture) {
-        this.moviePicture = moviePicture;
-    }
-
-    public Integer getRunningTime() {
-        return runningTime;
-    }
-
-    public void setRunningTime(Integer runningTime) {
-        this.runningTime = runningTime;
-    }
-
-    public Date getReleaseTime() {
-        return releaseTime;
-    }
-
-    public void setReleaseTime(Date releaseTime) {
-        this.releaseTime = releaseTime;
-    }
-
-    public String getStagePhotos() {
-        return stagePhotos;
-    }
-
-    public void setStagePhotos(String stagePhotos) {
-        this.stagePhotos = stagePhotos;
-    }
-
-    public String getPrevideo() {
-        return prevideo;
-    }
-
-    public void setPrevideo(String prevideo) {
-        this.prevideo = prevideo;
-    }
-
-    public Float getScore() {
-        return score;
-    }
-
-    public void setScore(Float score) {
-        this.score = score;
-    }
-
-    public Date getSoldOutTime() {
-        return soldOutTime;
-    }
-
-    public void setSoldOutTime(Date soldOutTime) {
-        this.soldOutTime = soldOutTime;
-    }
 }
--- huangniuniu-movie/huangniuniu-movie-service/src/main/java/com/huangniuniu/movie/controller/MovieController.java
+++ huangniuniu-movie/huangniuniu-movie-service/src/main/java/com/huangniuniu/movie/controller/MovieController.java
@@ -5,22 +5,23 @@ import com.huangniuniu.movie.service.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+
+import javax.validation.Valid;
 import java.util.List;
 
-@Controller
-@RequestMapping("movie")
+@RestController
 public class MovieController {
     @Autowired
     private MovieService movieService;
 
+    /**
+     * 查询电影信息
+     * @return
+     */
     @GetMapping("list")
     public ResponseEntity<List<Movie>> getAllMovie(){
         List<Movie> allMovie = this.movieService.getAllMovie();
@@ -31,6 +32,11 @@ public class MovieController {
 
     }
 
+    /**
+     * 根据电影id查询电影
+     * @param mid
+     * @return
+     */
     @GetMapping("getById/{mid}")
     public ResponseEntity<Movie> getMovieByMovieid(@PathVariable("mid") Integer mid){
         Movie movieByMovieid = this.movieService.getMovieByMovieid(mid);
@@ -41,27 +47,35 @@ public class MovieController {
     }
 
     @PostMapping("insert")
-    public ResponseEntity<Void> insertMovie(@RequestBody() Movie movie){
+    public ResponseEntity<Void> insertMovie(@Valid Movie movie){
         this.movieService.insertMovie(movie);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
-    @GetMapping("delete")
-    public ResponseEntity<Void> deleteMovie(@RequestBody() Movie movie){
-
-        this.movieService.deleteMovie(movie);
+    @DeleteMapping("delete/{id}")
+    public ResponseEntity<Void> deleteMovie(@PathVariable("id") Long id){
+        this.movieService.deleteMovie(id);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
+
     @GetMapping("getByCondition")
-    public ResponseEntity<List<Movie>> getMovieByCondition(@RequestBody() Movie movie){
-//        DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
-//
-//        movie.setSoldOutTime(new Date(df.format(movie.getReleaseTime())));
-//        movie.setReleaseTime(new Date(df.format(movie.getSoldOutTime())));
+    public ResponseEntity<List<Movie>> getMovieByCondition(Movie movie){
+
         List<Movie> movieByCondition = this.movieService.getMovieByCondition(movie);
         if(CollectionUtils.isEmpty(movieByCondition)){
             return ResponseEntity.notFound().build();
         }
         return ResponseEntity.ok(movieByCondition);
+    }
+
+    @GetMapping("getMovieByCityid")
+    public ResponseEntity<List<Movie>> getMovieByCityid(@RequestParam("cid") Long cid,@RequestParam("ishot") Boolean ishot){
+        List<Movie> movieByCityid = movieService.getMovieByCityid(cid, ishot);
+
+        if(CollectionUtils.isEmpty(movieByCityid)){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(movieByCityid);
+
     }
 
 
