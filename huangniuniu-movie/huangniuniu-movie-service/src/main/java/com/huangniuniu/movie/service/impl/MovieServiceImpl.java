6295588481950cc2255ef49cc19eea9dce5a0fe9
jc <2607339003@qq.com>
2019-10-12 15:57:58 +0800

    导个包

--- huangniuniu-movie/huangniuniu-movie-service/src/main/java/com/huangniuniu/movie/service/impl/MovieServiceImpl.java
+++ huangniuniu-movie/huangniuniu-movie-service/src/main/java/com/huangniuniu/movie/service/impl/MovieServiceImpl.java
@@ -15,7 +15,7 @@ import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.util.CollectionUtils;
 import tk.mybatis.mapper.entity.Example;
 
 import java.util.ArrayList;
